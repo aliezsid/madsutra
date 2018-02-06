29ee03fa484fc51e604db3f83a2464c194f8648d
Siddharth <sidgroks@gmail.com>
2018-02-06 22:38:07 +0530

    mobile version made

--- client/js/main.js
+++ client/js/main.js
@@ -9,3 +9,13 @@ document.addEventListener("scroll", function(e) {
 		siteLogo.style.height = "";
 	}
 });
+
+/* Open */
+function openNav() {
+	document.getElementById("nav").style.height = "100%";
+}
+
+/* Close */
+function closeNav() {
+	document.getElementById("nav").style.height = "0%";
+}
