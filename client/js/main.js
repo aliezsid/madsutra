16a5802d33b15522027b165fd1f0e3202ae1c322
Siddharth <sidgroks@gmail.com>
2018-02-06 21:47:11 +0530

    logo slide

---
+++ client/js/main.js
@@ -0,0 +1,10 @@
+const siteLogo = document.querySelector("#site-logo");
+
+document.addEventListener("scroll", function(e) {
+	if (window.scrollY > 100) {
+		siteLogo.style.height = "60px";
+		siteLogo.src = "./img/emblem.svg";
+	} else {
+		siteLogo.src = "./img/logo-text-min.png";
+	}
+});
