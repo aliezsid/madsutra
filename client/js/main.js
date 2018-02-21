cbeab710e4cc8ad450394a14f950766aece9ab46
Siddharth Gelera <sidgroks@gmail.com>
2018-02-21 12:07:22 +0530

    color and logo change

--- client/js/main.js
+++ client/js/main.js
@@ -3,7 +3,7 @@ const siteLogo = document.querySelector("#site-logo");
 document.addEventListener("scroll", function(e) {
 	if (window.scrollY > 100) {
 		siteLogo.style.height = "60px";
-		siteLogo.src = "./img/emblem.svg";
+		siteLogo.src = "./img/emblem.png";
 	} else {
 		siteLogo.src = "./img/logo-text-min.png";
 		siteLogo.style.height = "";
